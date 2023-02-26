00a685791cf304d1d181d68a077810434d3f7dba
anduamlak <andiialpha@gmail.com>
2023-02-26 10:19:07 +0300

    first comment

---
+++ storage/framework/views/5d3457add21a17727c3c6f398b8a9a5a6237bc09.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('title', __('Page Expired')); ?>
+<?php $__env->startSection('code', '419'); ?>
+<?php $__env->startSection('message', __('Page Expired')); ?>
+
+<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH F:\projects\qr-menu-maker-saas-v2.6.3\resources\views/errors/419.blade.php ENDPATH**/ ?>
